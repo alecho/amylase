e6688fd4255d810b49672e760505c72766d6af37
Andrew Lechowicz <andrew@alecho.com>
2017-11-30 20:36:09 -0500

    Run `mix new .`

---
+++ lib/amylase.ex
@@ -0,0 +1,18 @@
+defmodule Amylase do
+  @moduledoc """
+  Documentation for Amylase.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Amylase.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/amylase_test.exs
@@ -0,0 +1,8 @@
+defmodule AmylaseTest do
+  use ExUnit.Case
+  doctest Amylase
+
+  test "greets the world" do
+    assert Amylase.hello() == :world
+  end
+end
